5b6570fc2a22af508a91bf11783c2728ea0a03e8
ClavdiaJFA <cjfa08102013@gmail.com>
2021-07-16 12:21:23 -0500

    tryCatch

---
+++ TryCatch/src/Principal.java
@@ -0,0 +1,39 @@
+
+public class Principal {
+	/*
+	static int numerador = 10;
+	static Integer denominador = null;
+	static float division;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Antes de la división");
+		try {
+			division = numerador / denominador;
+		} catch (ArithmeticException error) {
+			division = 0;
+			System.out.println("Error: " + error.getMessage());
+		
+		} catch(NullPointerException ex) {
+			division = 0; 
+			System.out.println("Error: " + ex.getMessage());
+			System.out.println("Segundo catch");
+		}  finally {
+		
+		System.out.println("Después de la división");
+		}
+		*/
+		
+		String frutas [] = {"Mango", "Pera", "Manzana", "Uva"};
+		
+		//foreach en Java
+		for (String fruta: frutas) {
+			System.out.println(fruta);
+			 
+		}
+		
+
+	}
+
+}
